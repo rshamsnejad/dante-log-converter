1ec5faaa8372cca16a6c0acd3ae32a3777489d2c
Robin SHAMSNEJAD <rsham@free.fr>
2025-05-27 18:18:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dante Log Converter
+
+This script converts log files from Dante Controller into CSV.
+
+Usage : `convert-dante-log-to-csv.py <log file>`
---
+++ convert-dante-log-to-csv.py
@@ -0,0 +1,48 @@
+import os.path
+import csv
+import re
+import sys
+
+if len(sys.argv) != 2:
+    print("Wrong arguments !")
+    sys.exit(1)
+
+log_filename = sys.argv[1]
+csv_filename = os.path.splitext(log_filename)[0] + ".csv"
+
+try:
+
+    log_file = open(log_filename, 'r')
+    csv_file = open(csv_filename, 'w', newline='')
+
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["Date", "Timestamp", "Log Level", "Device", "Message"])
+
+    # First line is useless, skip it
+    next(log_file)
+
+    for line in log_file:
+
+        row_split = re.split(r'\s', line.strip(), maxsplit=6)
+        
+        row = [
+            f"{row_split[0]} {row_split[1]} {row_split[2]}",
+            row_split[3],
+            row_split[4],
+            row_split[5].strip('"'),
+            row_split[6].strip('"'),
+        ]
+
+        # In case the device field is empty, fill it with the MAC address from the message
+        if not row[3]:
+            message_split = re.split(r'(device name not known)',row[4])[0].strip()
+
+            row[3] = message_split.split(' ')[1]
+
+        csv_writer.writerow(row)
+
+    log_file.close()
+    csv_file.close()
+
+except Exception as e:
+    print(e)
